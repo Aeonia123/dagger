2a4355e3b37e979908836a17ffaafeee0d5e4c44
Aeonia123 <158709740+Aeonia123@users.noreply.github.com>
2024-02-04 09:53:11 -0800

    better parsing

--- get_chat_id.py
+++ get_chat_id.py
@@ -10,20 +10,30 @@ from telegram import Bot
 bot_token = 'your_bot_token'
 
 async def get_chat_id(bot_token):
+  chat_ids = []
   try:
     bot = Bot(token=bot_token)
     updates = await bot.get_updates()
-    if updates:
-      chat_id = updates[0].message.chat_id
-      return chat_id
+
+    for update in updates:
+      if hasattr(update, "my_chat_member") and update.my_chat_member is not None:  
+        chat_id = update.my_chat_member.chat.id
+        chat_ids.append(chat_id)
+      if hasattr(update, "message") and update.message is not None:
+          chat_id = update.message.chat.id
+          chat_ids.append(chat_id)
+
   except Exception as e:
     print(f"An error occurred: {str(e)}")
-  return None
+
+  # Remove duplicates
+  return set(chat_ids)
 
 async def main():
-  chat_id = await get_chat_id(bot_token)
-  if chat_id:
-    print(f"Your chat ID is: {chat_id}")
+  chat_ids = await get_chat_id(bot_token)
+  if chat_ids:
+    for chat_id in chat_ids:
+      print(f"Your chat ID is: {chat_id}")
   else:
     print("Failed to retrieve chat ID.")
 
